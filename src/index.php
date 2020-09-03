f506f3a4229b5e3fd88f525c7d8bc73f7b769c59
takahiro <takahiro.tsuchiya@sold-out.co.jp>
2020-09-03 12:00:10 +0900

    aタグの処理を追加

--- src/index.php
+++ src/index.php
@@ -15,12 +15,19 @@ require_once("./phpQuery-onefile.php");
 $html = file_get_contents("https://ja.wikipedia.org/wiki/%E3%82%A6%E3%82%A7%E3%83%96%E3%82%B9%E3%82%AF%E3%83%AC%E3%82%A4%E3%83%94%E3%83%B3%E3%82%B0");
 $dom = phpQuery::newDocument($html);
 
+// h1タグを取り出す
 $h1 = $dom->find("h1")->text();
 echo $h1 . '<br/>';
 
+// titleタグの中身を取り出す
 $title = $dom->find("title")->text();
 echo $title . '<br/>';
 
+// aタグを取り出す
+foreach ($dom->find('a') as $a){
+$a = $a->getAttribute('href');
+echo '<a href=' . $a . '>' . $a . '</a><br/>';
+}
 
 ?>
 </body>
