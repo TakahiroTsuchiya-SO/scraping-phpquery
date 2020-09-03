15d64f0e6a4941a71a78bbd4632d2518ff28adf8
takahiro <takahiro.tsuchiya@sold-out.co.jp>
2020-09-03 10:47:16 +0900

    first commit

--- src/index.php
+++ src/index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="utf-8">
+<title>PHP100本ノック</title>
+</head>
+<body>
 <?php
- phpinfo();
+header('Content-Type: text/html; charset=UTF-8');
+ini_set("display_errors", 1);
+error_reporting(E_ALL);
+mb_internal_encoding("UTF-8");
+
+echo "hello";
 ?>
+</body>
+</html>
